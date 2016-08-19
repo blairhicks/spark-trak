788d06842be257c3fe2e346aaa13c811d86f74e7
Blair Hicks <blahicks@cisco.com>
2016-08-19 10:28:09 -0500

    More testing - fix typo in server.js to announce presence

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var config = {
    // spark account email
     sparkEmail: myBotEmail,
     // spark api token
-    sparkToken: token_spark
+    sparkToken: token_spark, 
     //sparkToken: token_spark,
     // bot ready message
     announceMessage: 'I am alive and well.  How may I help you today? (For some ideas, type /help)'
